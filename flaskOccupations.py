536935d5f4b6da4c93277c4c8e30ae3031202b14
GordonLei <glei@stuy.edu>
2017-09-28 23:04:15 -0400

    reformatted
    reformat for flask apps

--- flaskOccupations.py
+++ flaskOccupations.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from occupation import randomJob, createDict, returnReader
+from utils import occupation
 #import occupation, csv, random
 
 app = Flask(__name__)
@@ -9,8 +9,8 @@ def root():
         return "root"
 
 @app.route("/occupations")
-def occupation(): 
-        return render_template("ace.html", dictJob = createDict(), randJob = randomJob(createDict()))
+def occupations(): 
+        return render_template("ace.html", dictJob = occupation.createDict(), randJob = occupation.randomJob(occupation.createDict()))
 
 if __name__ == "__main__":
         app.debug = True
